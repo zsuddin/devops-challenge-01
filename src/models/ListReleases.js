6e004ff0d511fee0b031d7fa0885c8b079536559
Zain Uddin <izainuddin@outlook.com>
2024-10-06 14:35:06 +0100

    [DEVOPS-126] add validations

--- src/models/ListReleases.js
+++ src/models/ListReleases.js
@@ -1,8 +1,20 @@
+const validator = require('validator');
+
 class ListReleases {
     constructor({ limit = 10, offset = 0 }) {
         this.limit = parseInt(limit, 10);
         this.offset = parseInt(offset, 10);
+        this.limit = this.sanitizeLimit(limit);
+        this.offset = this.sanitizeOffset(offset);
+    }
+
+    sanitizeLimit(limit) {
+        const sanitizedLimit = validator.toInt(validator.trim(String(limit)), 10);
+        return sanitizedLimit >= 0 ? sanitizedLimit : 10;
+    }
+
+    sanitizeOffset(offset) {
+        const sanitizedOffset = validator.toInt(validator.trim(String(offset)), 10);
+        return sanitizedOffset >= 0 ? sanitizedOffset : 0;
     }
 }
-
-module.exports = ListReleases;
